23e46277f4a81f6a48464f15470eaf5f4116cfae
brcre001 <brandon.f.creed@gmail.com>
2021-06-02 03:23:18 +0000

    Majority of Program working

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Arrays with Values
+
+  const pronoun = ["the", "our", "my", "their", "his"];
+  const adj = ["last", "greatest", "biggest", "smallest", "silliest"];
+  const noun = ["elon", "gates", "jobbs", "bezos", "mogul"];
+  const ending = [".com", ".net", ".us", ".io", ".biz"];
+
+  // Array that will contain list of generated domain names
+  let finalOutput = [];
+
+  // Loop to print 10 different combinations
+  for (let i = 0; i < 10; i++) {
+    let indexPronoun = Math.floor(Math.random() * pronoun.length);
+    let indexAdj = Math.floor(Math.random() * adj.length);
+    let indexNoun = Math.floor(Math.random() * noun.length);
+    let indexEnding = Math.floor(Math.random() * ending.length);
+
+    // Get string for domain name
+    let sentence =
+      pronoun[indexPronoun] +
+      adj[indexAdj] +
+      noun[indexNoun] +
+      ending[indexEnding];
+
+    // Pushing generated string to an separate array
+    finalOutput.push(sentence);
+  }
+
+  for (let j = 0; j < 10; j++) {
+    console.log(finalOutput[j]);
+  }
+  document.getElementById("generator").innerHTML = finalOutput;
 };
